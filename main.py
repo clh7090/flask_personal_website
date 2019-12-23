4a6ee500ffd94f3daaf1bf08bcd89bc5e1d155e8
Connor <clh7090@rit.edu>
2019-12-23 16:21:21 -0500

    First outline/setup

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+author: Connor Hunter   <clh7090@rit.edu>
+
+Flask Personal Website
+"""
+
+
+from flask import Flask, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
